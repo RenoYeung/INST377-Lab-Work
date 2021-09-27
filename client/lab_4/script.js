91036f525fb219bac67175d6c0082727c3c4cd80
RenoYeung <ryeung12@umd.edu>
2021-09-27 12:53:33 -0400

    d

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -5,20 +5,26 @@ const totalSlides = slides.length;
 document.querySelector('#carousel_button--next')
 .addEventListener("click",function(){
     moveToNextSlide();
+    console.log('test');
 });
 
 document.querySelector('#carousel_button--prev')
 .addEventListener("click",function(){
     moveToPrevSlide();
+    console.log('test');
 });
 
 function updateSlidePostion(){
     for(let slide of slides){
-        slide.classList.remove('.photo-visible');
-        slide.classList.add('.photo-grid-item--hidden')
+        slide.classList.remove('photo-visible');
+        slide.classList.add('photo-grid-item--hidden')
     }
+    slides[slidePosition].classList.add('photo-visible');
+    slides[slidePosition].classList.remove('photo-grid-item--hidden');
+    console.log(slides);
+    console.log(slidePosition);
 }
-slides[slidePosition].classList.add('.photo-visible');
+
 
 function moveToNextSlide(){
     if(slidePosition === totalSlides - 1){
